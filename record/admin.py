a8e22bef431bf620fda9123549f1cc19ad782eb8
bobjiangps <jbsv43@sina.com>
2020-07-02 15:11:36 +0800

    update models, registered in admin

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tag, AliasName, Career, Address, Phone, QNum, WeChat, AliPay, WeiBo, Email, Person
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Person)
+admin.site.register(AliasName)
+admin.site.register(Career)
+admin.site.register(Address)
+admin.site.register(Phone)
+admin.site.register(QNum)
+admin.site.register(WeChat)
+admin.site.register(WeiBo)
+admin.site.register(AliPay)
+admin.site.register(Email)
