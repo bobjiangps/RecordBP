24eb3ff30b7e839b0098a24c50b4d060af9b9a9f
bobjiangps <jbsv43@sina.com>
2020-07-02 18:40:49 +0800

    add basic view to test template

--- record/views.py
+++ record/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request):
+    # port = request.META.get("SERVER_PORT")
+    # record_visit(request, page_suffix=f"/port={port}")
+    return render(request, 'record/main.html')
