48d4cdab7e88d94b9cb1b1c341fcb3dc828f5209
bobjiangps <jbsv43@sina.com>
2021-03-08 15:41:46 +0800

    when view file content, no need to login

--- files/views.py
+++ files/views.py
@@ -4,14 +4,21 @@ import os
 
 
 def show_file_content(request, filename):
-    user = request.user
-    if user.is_authenticated:
-        file_obj = Files.objects.filter(name=filename)
-        if len(file_obj) > 0:
-            current_file = file_obj[0]
-            with open(os.path.join(current_file.path, filename), "r") as f:
-                return HttpResponse(f'<pre>{current_file.content}</pre>')
-        else:
-            return HttpResponse("找不到文件")
+    file_obj = Files.objects.filter(name=filename)
+    if len(file_obj) > 0:
+        current_file = file_obj[0]
+        with open(os.path.join(current_file.path, filename), "r") as f:
+            return HttpResponse(f'<pre>{current_file.content}</pre>')
     else:
-        return HttpResponse("未登录")
+        return HttpResponse("找不到文件")
+    # user = request.user
+    # if user.is_authenticated:
+    #     file_obj = Files.objects.filter(name=filename)
+    #     if len(file_obj) > 0:
+    #         current_file = file_obj[0]
+    #         with open(os.path.join(current_file.path, filename), "r") as f:
+    #             return HttpResponse(f'<pre>{current_file.content}</pre>')
+    #     else:
+    #         return HttpResponse("找不到文件")
+    # else:
+    #     return HttpResponse("未登录")
